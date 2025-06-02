686d4eec795db2c6906d421adfb5afb8c6153e57
Samarth Saxena <samarthsaxena1672003@gmail.com>
2025-06-02 12:45:01 +0000

    socials fix:

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -33,6 +33,10 @@ const modal = createAppKit({
   themeVariables: {
     '--w3m-accent': '#121212',
     '--w3m-border-radius-master': '0.5px'
+  },
+  features:{
+    email:false,
+    socials:false
   }
 })
 
